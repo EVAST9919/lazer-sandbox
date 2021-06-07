5eddc4f07edf173ac8259298673a682d94ae29ca
Andrei Zavatski <megaman9919@gmail.com>
2021-06-07 04:47:37 +0300

    Add smoother animations to ruleset screens

--- osu.Game.Rulesets.Sandbox/Screens/SandboxScreen.cs
+++ osu.Game.Rulesets.Sandbox/Screens/SandboxScreen.cs
@@ -1,4 +1,5 @@
-﻿using osu.Framework.Screens;
+﻿using osu.Framework.Graphics;
+using osu.Framework.Screens;
 using osu.Game.Beatmaps;
 using osu.Game.Screens.Play;
 
@@ -16,7 +17,26 @@ namespace osu.Game.Rulesets.Sandbox.Screens
         public override void OnEntering(IScreen last)
         {
             base.OnEntering(last);
+            this.FadeInFromZero(250, Easing.OutQuint);
             updateComponentFromBeatmap(Beatmap.Value);
+        }
+
+        public override bool OnExiting(IScreen next)
+        {
+            this.FadeOut(250, Easing.OutQuint);
+            return base.OnExiting(next);
+        }
+
+        public override void OnResuming(IScreen last)
+        {
+            base.OnResuming(last);
+            this.FadeIn(250, Easing.OutQuint);
+        }
+
+        public override void OnSuspending(IScreen next)
+        {
+            base.OnSuspending(next);
+            this.FadeOut(250, Easing.OutQuint);
         }
 
         private void updateComponentFromBeatmap(WorkingBeatmap beatmap)
