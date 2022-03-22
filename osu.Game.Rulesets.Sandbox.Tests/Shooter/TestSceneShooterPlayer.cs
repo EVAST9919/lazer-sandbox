ac3220a319363928aa55607fd3f566a13a46fac3
Andrei Zavatski <megaman9919@gmail.com>
2022-03-22 17:02:13 +0300

    Implement ShooterPlayer

---
+++ osu.Game.Rulesets.Sandbox.Tests/Shooter/TestSceneShooterPlayer.cs
@@ -0,0 +1,13 @@
+﻿using osu.Game.Rulesets.Sandbox.Screens.Shooter;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Sandbox.Tests.Shooter
+{
+    public class TestSceneShooterPlayer : OsuTestScene
+    {
+        public TestSceneShooterPlayer()
+        {
+            Add(new ShooterPlayer());
+        }
+    }
+}
