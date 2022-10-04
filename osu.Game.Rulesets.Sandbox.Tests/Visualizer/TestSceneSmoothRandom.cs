eba0b93ee939e9576c096dc21953d182d6ac72b5
Andrei Zavatski <megaman9919@gmail.com>
2022-10-05 01:11:30 +0300

    Add test scene for smooth random

---
+++ osu.Game.Rulesets.Sandbox.Tests/Visualizer/TestSceneSmoothRandom.cs
@@ -0,0 +1,42 @@
+﻿using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.Sandbox.Extensions;
+using osu.Game.Tests.Visual;
+using osu.Framework.Graphics;
+using osuTK;
+
+namespace osu.Game.Rulesets.Sandbox.Tests.Visualizer
+{
+    public class TestSceneSmoothRandom : OsuTestScene
+    {
+        private readonly OsuSpriteText text;
+        private readonly OpenSimplexNoise noise;
+        private readonly Circle circle;
+
+        public TestSceneSmoothRandom()
+        {
+            noise = new OpenSimplexNoise();
+
+            Children = new Drawable[]
+            {
+                text = new OsuSpriteText(),
+                circle = new Circle
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(20)
+                }
+            };
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            var value = noise.Evaluate(Time.Current / 1000, 0.0);
+
+            text.Text = value.ToString();
+            circle.Y = (float)value * 100;
+        }
+    }
+}
